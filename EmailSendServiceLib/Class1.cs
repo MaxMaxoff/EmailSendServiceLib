11353f26c7045762faa0403ef3963772510a1e76
Maxim VT <maxim.vt@gmail.com>
2019-02-10 15:58:51 +0300

    Add project files.

---
+++ EmailSendServiceLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSendServiceLib
+{
+    public class Class1
+    {
+    }
+}
